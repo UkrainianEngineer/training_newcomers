f615be1a34a73edb80e67896cbae46974af2621b
Vasyl Varyshchuk <vasyl.varyshchuk@globallogic.com>
2018-08-09 16:29:00 +0300

    Improove code quality

--- vasyl_varyshchuk/Week_1/task_1/max_of_list.py
+++ vasyl_varyshchuk/Week_1/task_1/max_of_list.py
@@ -1,28 +1,23 @@
-'''
-This module finds maximum integer value from list of strings.
-
-'''
+"""
+This module finds maximum integer value from the list of strings.
+"""
 
 data = ['one', '1', 'two', '2', 'ten', '10', '15', '-2', 'some long text goes here', '16.9', '&']
+
 
 def get_max_value(iterable):
 
     numbers = []
-
     for item in iterable:
-
         try:
             numbers.append(int(float(item)))
         except ValueError:
             print("Could not convert '{}' to an integer.".format(item))
 
-    if len(numbers) != 0:
-        return max(numbers)
-    else:
-        return None
+    return max(numbers) if numbers else None
 
 
 result = get_max_value(data)
-print('\n' +  'Maximum integer value of list: {}:'.format(result))
+print('\n' + 'Maximum integer value of list: {}:'.format(result))
 
 
--- vasyl_varyshchuk/Week_1/task_1/test_get_max_value.py
+++ vasyl_varyshchuk/Week_1/task_1/test_get_max_value.py
@@ -1,8 +1,19 @@
+"""
+This is module for testing get_max_value() function.
+"""
 from max_of_list import get_max_value
 
 
 def test_get_max_value():
 
-    assert get_max_value(['5', 'some text', 'nine', '9', '3.5', '12k']) == 9
+    test_data_1 = ['5', 'some text', 'nine', '9', '3.5', '12k', '9']
+    test_data_2 = ['five', 'two', 'seven', 'ten', 'three']
 
-    assert get_max_value(['hkk', 'one', 'fgdg']) == None
+    assert get_max_value(test_data_1) == 9
+    assert get_max_value(test_data_2) is None
+
+
+def test_float_values():
+
+    test_data_3 = ['25.45', '376.45', '55.3337', '782.563', '54.5', '782']
+    assert get_max_value(test_data_3) == 782
