62cc9c645c0acb5f561f0520d795f9c8a567aac2
Vasyl Varyshchuk <vasyl.varyshchuk@globallogic.com>
2018-08-10 13:46:04 +0300

    Change variables name in test_max_value_module

--- vasyl_varyshchuk/Week_1/task_1/test_get_max_value.py
+++ vasyl_varyshchuk/Week_1/task_1/test_get_max_value.py
@@ -6,18 +6,18 @@ from max_of_list import get_max_value
 
 def test_positive():
 
-    test_data_1 = ['5', 'some text', 'nine', '9', '3.5', '12k', '9']
-    assert get_max_value(test_data_1) == 9
+    test_data = ['5', 'some text', 'nine', '9', '3.5', '12k', '9']
+    assert get_max_value(test_data) == 9
 
 
 def test_negative():
 
-    test_data_2 = ['five', 'two', 'seven', 'ten', 'three']
-    assert get_max_value(test_data_2) is None
+    test_data = ['five', 'two', 'seven', 'ten', 'three']
+    assert get_max_value(test_data) is None
 
 
 
 def test_float_values():
 
-    test_data_3 = ['25.45', '376.45', '55.3337', '54.5', '782,3']
-    assert get_max_value(test_data_3) == 782
+    test_data = ['25.45', '376.45', '55.3337', '54.5', '782.3']
+    assert get_max_value(test_data) == 782
