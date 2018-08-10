70e602b9ed2fd0f51580a5736517bba15091a5e1
Vasyl Varyshchuk <vasyl.varyshchuk@globallogic.com>
2018-08-10 11:02:47 +0300

    Add solutions for task_2

--- vasyl_varyshchuk/Week_1/task_1/test_get_max_value.py
+++ vasyl_varyshchuk/Week_1/task_1/test_get_max_value.py
@@ -4,16 +4,20 @@ This is module for testing get_max_value() function.
 from max_of_list import get_max_value
 
 
-def test_get_max_value():
+def test_positive():
 
     test_data_1 = ['5', 'some text', 'nine', '9', '3.5', '12k', '9']
-    test_data_2 = ['five', 'two', 'seven', 'ten', 'three']
-
     assert get_max_value(test_data_1) == 9
+
+
+def test_negative():
+
+    test_data_2 = ['five', 'two', 'seven', 'ten', 'three']
     assert get_max_value(test_data_2) is None
+
 
 
 def test_float_values():
 
-    test_data_3 = ['25.45', '376.45', '55.3337', '782.563', '54.5', '782']
+    test_data_3 = ['25.45', '376.45', '55.3337', '54.5', '782,3']
     assert get_max_value(test_data_3) == 782
