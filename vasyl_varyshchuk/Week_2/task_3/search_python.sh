f6659d134db436570b7ba4d48dd5fe2f093f58da
Vasyl Varyshchuk <vasyl.varyshchuk@globallogic.com>
2018-08-29 14:37:50 +0300

    Added description to serch_python.sh script

--- vasyl_varyshchuk/Week_2/task_3/search_python.sh
+++ vasyl_varyshchuk/Week_2/task_3/search_python.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-# Search python processes
+
+# This script searches for running processes key_word pattern passed from command line
+# ps command in combination with -efl options creates a table with information for each processes.
+# After that, grep finds lines containing 'Skey_word' pattern, whitch is passed from command line.
+# Finally awk prints colomn which corresponds to PID's, excluding lines which conatains 'bash' or 
+# 'grep' to display only python processes.
 
 key_word=$1
 
