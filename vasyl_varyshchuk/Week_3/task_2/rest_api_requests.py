53ea00d9f01b85d363aee94065fe0e1b07bb5465
Vasyl Varyshchuk <vasyl.varyshchuk@globallogic.com>
2018-09-06 18:40:18 +0300

    Changed errors handling

--- vasyl_varyshchuk/Week_3/task_2/rest_api_requests.py
+++ vasyl_varyshchuk/Week_3/task_2/rest_api_requests.py
@@ -8,26 +8,42 @@ URL = 'http://127.0.0.1:5000/api/'
 headers = {'Content-type': 'application/json'}
 
 
+def catch_response_exceptions(func):
+    """A decorator that wraps the passed in functions and catches
+    requests exceptions which can occur"""
+    def wrapper(*args):
+        try:
+            return func(*args)
+        except requests.exceptions.ConnectionError as error:
+            print('Connection error:', error.message)
+        except requests.exceptions.HTTPError as error:
+            print('HTTP error:', error.message)
+        except requests.exceptions.InvalidSchema as error:
+            print('Invalid Schema error:', error.message)
+        except requests.exceptions.Timeout as error:
+            print('Timeout error:', error.message)
+        except requests.exceptions.RequestException as error:
+            print('Unexpected error:', error.message)
+    return wrapper
+
+
+@catch_response_exceptions
 def get_list_of_users():
     """This function returns list of users from a server"""
-    try:
-        response = requests.get(URL+'users/')
-        response.raise_for_status()
-        return response.json()['users']
-    except requests.RequestException as error:
-        print(error)
+    response = requests.get(URL+'users/', timeout=10)
+    response.raise_for_status()
+    return response.json()['users']
 
 
+@catch_response_exceptions
 def get_concrete_user(user_id):
     """This function gets information about concrete user from server by user_id"""
-    try:
-        response = requests.get(URL+'users/{}/'.format(user_id))
-        response.raise_for_status()
-        return response.json()['user']
-    except requests.RequestException as error:
-        print (error)
+    response = requests.get(URL+'users/{}/'.format(user_id), timeout=5)
+    response.raise_for_status()
+    return response.json()['user']
 
 
+@catch_response_exceptions
 def add_new_user(new_user_info):
     """
     This function adds new user to the server passing a dictionary of information about user.
@@ -35,53 +51,42 @@ def add_new_user(new_user_info):
         {'name': 'user_name', 'surname': 'user_surname', 'age': user_age}
     """
     new_user_json = json.dumps(new_user_info)
-    try:
-        response = requests.post(URL+'users/', data=new_user_json, headers=headers)
-        response.raise_for_status()
-        print('New user was successfully added')
-    except requests.RequestException as error:
-        print(error)
+    response = requests.post(URL+'users/', data=new_user_json, headers=headers, timeout=5)
+    response.raise_for_status()
+    print('New user was successfully added')
 
 
+@catch_response_exceptions
 def update_user_info(user_id, update_info):
     """
     This function updates information about current user.
     update_info -  dictionary with new information about user
     """
-    update_info_json = json.dumps(update_info)
-    user_url = URL + 'users/{}/'.format(user_id)
-    try:
-        get_user_response = requests.get(user_url)
-        put_response = requests.put(user_url, data=update_info_json, headers=headers)
-        put_response.raise_for_status()
-        if get_user_response.json()['user'] is not None:
-            print('Information was updated for user with id={}'.format(user_id))
-        else:
-            print('User with id={} does not exist'.format(user_id))
-    except requests.RequestException as error:
-        print(error)
+    if get_concrete_user(user_id) is not None:
+        update_info_json = json.dumps(update_info)
+        response = requests.put(URL+'users/{}/'.format(user_id), data=update_info_json, headers=headers, timeout=5)
+        response.raise_for_status()
+        print('Information was updated for user with id={}'.format(user_id))
+    else:
+        print('User with id={} does not exist'.format(user_id))
 
 
+@catch_response_exceptions
 def delete_user(user_id):
     """This function deletes user from server passing user id"""
-    user_url = URL+'users/{}/'.format(user_id)
-    try:
-        get_user_response = requests.get(user_url)
-        delete_response = requests.delete(user_url)
-        delete_response.raise_for_status()
-        if get_user_response.json()['user'] is not None:
-            print('User with id={} was deleted'.format(user_id))
-        else:
-            print('User with id={} does not exist'.format(user_id))
-    except requests.RequestException as error:
-        print(error)
+    if get_concrete_user(user_id) is not None:
+        response = requests.delete(URL+'users/{}/'.format(user_id), timeout=5)
+        response.raise_for_status()
+        print('User with id={} was deleted'.format(user_id))
+    else:
+        print('User with id={} does not exist'.format(user_id))
 
 
 # get all users from server
 print(get_list_of_users())
 
 # get information about concrete user
-print(get_concrete_user(5))
+print(get_concrete_user(2))
 
 # add new user to the server
 new_user = {'name': 'Vasyl', 'surname': 'Varyshchuk', 'age': 28}
