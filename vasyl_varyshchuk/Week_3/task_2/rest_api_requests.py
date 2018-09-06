8218e4b54fdb26e4cf6a83c1293f2834314dd4ef
Vasyl Varyshchuk <vasyl.varyshchuk@globallogic.com>
2018-09-06 14:05:14 +0300

    Added try/except to catch exception

--- vasyl_varyshchuk/Week_3/task_2/rest_api_requests.py
+++ vasyl_varyshchuk/Week_3/task_2/rest_api_requests.py
@@ -1,35 +1,31 @@
-""""
+"""
 This script contains functions for display, create, update and delete users from server with REST API
 """
 import requests
 import json
 
-url = 'http://127.0.0.1:5000/api/users/'
+URL = 'http://127.0.0.1:5000/api/'
 headers = {'Content-type': 'application/json'}
 
 
 def get_list_of_users():
     """This function returns list of users from a server"""
-    response = requests.get(url)
-    if response.status_code == 200:
+    try:
+        response = requests.get(URL+'users/')
+        response.raise_for_status()
         return response.json()['users']
-    else:
-        print('Something went wrong', response.text)
-    print('------------------------------------------')
+    except requests.RequestException as error:
+        print(error)
 
 
 def get_concrete_user(user_id):
     """This function gets information about concrete user from server by user_id"""
-    response = requests.get('http://127.0.0.1:5000/api/users/{}/'.format(user_id))
-    if response.status_code == 200:
-        if response.json()['user'] is not None:
-            return response.json()
-        else:
-            print('User with id={} does not exist'.format(user_id))
-            print('------------------------------------------')
-    else:
-        print('Something went wrong with response from server.', response.status_code, response.text)
-        print('------------------------------------------')
+    try:
+        response = requests.get(URL+'users/{}/'.format(user_id))
+        response.raise_for_status()
+        return response.json()['user']
+    except requests.RequestException as error:
+        print (error)
 
 
 def add_new_user(new_user_info):
@@ -39,12 +35,12 @@ def add_new_user(new_user_info):
         {'name': 'user_name', 'surname': 'user_surname', 'age': user_age}
     """
     new_user_json = json.dumps(new_user_info)
-    response = requests.post(url, data=new_user_json, headers=headers)
-    if response.status_code == 200:
-        print('New user was successfully added.')
-    else:
-        print('Something went wrong with response from server.', response.status_code, response.text)
-    print('------------------------------------------')
+    try:
+        response = requests.post(URL+'users/', data=new_user_json, headers=headers)
+        response.raise_for_status()
+        print('New user was successfully added')
+    except requests.RequestException as error:
+        print(error)
 
 
 def update_user_info(user_id, update_info):
@@ -52,33 +48,40 @@ def update_user_info(user_id, update_info):
     This function updates information about current user.
     update_info -  dictionary with new information about user
     """
-    if get_concrete_user(user_id) is not None:
-        url = 'http://127.0.0.1:5000/api/users/{}/'.format(user_id)
-        update_info_json = json.dumps(update_info)
-        response = requests.put(url, data=update_info_json, headers=headers)
-        if response.status_code == 200:
+    update_info_json = json.dumps(update_info)
+    user_url = URL + 'users/{}/'.format(user_id)
+    try:
+        get_user_response = requests.get(user_url)
+        put_response = requests.put(user_url, data=update_info_json, headers=headers)
+        put_response.raise_for_status()
+        if get_user_response.json()['user'] is not None:
             print('Information was updated for user with id={}'.format(user_id))
         else:
-            print('Something went wrong.', response.status_code, response.text)
-        print('------------------------------------------')
+            print('User with id={} does not exist'.format(user_id))
+    except requests.RequestException as error:
+        print(error)
 
 
 def delete_user(user_id):
     """This function deletes user from server passing user id"""
-    if get_concrete_user(user_id) is not None:
-        response = requests.delete('http://127.0.0.1:5000/api/users/{}/'.format(user_id))
-        if response.status_code == 200:
+    user_url = URL+'users/{}/'.format(user_id)
+    try:
+        get_user_response = requests.get(user_url)
+        delete_response = requests.delete(user_url)
+        delete_response.raise_for_status()
+        if get_user_response.json()['user'] is not None:
             print('User with id={} was deleted'.format(user_id))
         else:
-            print('Something went wrong.', response.status_code, response.text)
-        print('------------------------------------------')
+            print('User with id={} does not exist'.format(user_id))
+    except requests.RequestException as error:
+        print(error)
 
 
 # get all users from server
 print(get_list_of_users())
 
 # get information about concrete user
-print(get_concrete_user(2))
+print(get_concrete_user(5))
 
 # add new user to the server
 new_user = {'name': 'Vasyl', 'surname': 'Varyshchuk', 'age': 28}
@@ -90,4 +93,3 @@ update_user_info(2, updated_user_info)
 
 # delete an existing user
 delete_user(3)
-
